77d96b5278d41f100bb551cd27645eb135044df8
taranovev <tar_ev@mail.ru>
2022-10-31 09:00:34 +0300

    Add Multiline strings Add sleep time Put read_envin main.py

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
 import argparse
 import requests
 import telegram
+
+from textwrap import dedent
+from time import sleep
 from environs import Env
 from requests.exceptions import ConnectionError, ReadTimeout
 
 
-env = Env()
-env.read_env()
-TELEGRAM_TOKEN = env.str('TELEGRAM_TOKEN')
-DEVMAN_TOKEN = env.str('DEVMAN_TOKEN')
-
-
 def main():
+    env = Env()
+    env.read_env()
+    telegram_token = env.str('TELEGRAM_TOKEN')
+    devman_token = env.str('DEVMAN_TOKEN')
     parser = argparse.ArgumentParser()
     parser.add_argument('chat_id',
                         help='id чата, куда бот будет присылать сообщения',
                         type=int)
     args = parser.parse_args()
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(token=telegram_token)
     params = {}
     url = 'https://dvmn.org/api/long_polling/'
-    headers = {
-                'Authorization': DEVMAN_TOKEN
-            }
+    headers = {'Authorization': devman_token}
+
     while True:
         try:
             response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
-            response_json = response.json()
-            if response_json['status'] == 'timeout':
-                params = {'timestamp': response_json['timestamp_to_request']}
+            response_data = response.json()
+            if response_data['status'] == 'timeout':
+                params = {'timestamp': response_data['timestamp_to_request']}
             else:
-                params = {'timestamp': response_json['last_attempt_timestamp']}
-                attempt = response_json['new_attempts'][0]
+                params = {'timestamp': response_data['last_attempt_timestamp']}
+                attempt = response_data['new_attempts'][0]
                 if attempt['is_negative']:
                     decision = 'К сожалению, в работе нашлись ошибки =('
                 else:
                     decision = 'Замечаний нет =)'
                 text = (
-                        f'''Урок "{attempt['lesson_title']}" проверен
-                        \n{decision}
-                        \nПодробнее: {attempt['lesson_url']}'''
+                        f'''
+                        Урок "{attempt['lesson_title']}" проверен
+                        {decision}
+                        Подробнее: {attempt['lesson_url']}
+                        '''
                 )
 
-                bot.send_message(text=text, chat_id=args.chat_id)
+                bot.send_message(text=dedent(text), chat_id=args.chat_id)
         except (ReadTimeout, ConnectionError):
+            sleep(60)
             continue
 
 
